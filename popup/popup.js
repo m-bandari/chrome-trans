5e3179b01256a21665d627a4276afb1ce523e4b3
Mahesh Bandari <11704574+m-bandari@users.noreply.github.com>
2020-02-17 12:05:03 +0530

    Added Support for JET UI

--- popup/popup.js
+++ popup/popup.js
@@ -1,45 +1,74 @@
-chrome.tabs.executeScript({file: "/manifest.js"});
 var tranId;
+var tranIdLegacy;
 var procId;
-chrome.runtime.onMessage.addListener((message => { //gets message from content_script, manifest.js
-	tranId = message.value;
-	document.getElementById("buysideid").value = tranId;
-	procId = message.value2;
-}))
 
+chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+  var port = chrome.tabs.connect(tabs[0].id); //Setting up connection with content script
+  currentTabUrl = tabs[0].url; //getting the current url of the active tab to retrieve the domain name
+  if (
+    currentTabUrl.includes(".bigmachines.com/commerce/buyside/document.jsp") //Determine if the quote id using legacy or JET UI
+  ) {
+    port.postMessage({ isLegacy: "true" }); //Requesting for transaction ID and process ID based on the flag
+  } else if (currentTabUrl.includes(".bigmachines.com/commerce/transaction")) {
+    port.postMessage({ isLegacy: "false", tabUrl: currentTabUrl });
+    currentTabUrlFragmentsArr = currentTabUrl.split("/");
+    tranId = currentTabUrlFragmentsArr[currentTabUrlFragmentsArr.length - 1];
+    document.getElementById("buysideid").value = tranId;
+  }
 
-function openXml(xslUrl,subdomain) {
-	
-	var xhttp = new XMLHttpRequest();			//Ajax call to get xslt_id from the views page 
-	xhttp.onreadystatechange = function() {
-		if(this.readyState == 4 && this.status == 200) {	
-			var data = this.responseText;
-			var toSearch = "edit_xslt.jsp?id=";
-			var pos = data.indexOf(toSearch);
-			var xslIdStr = data.substr(pos+toSearch.length,50);
-			var newpos = xslIdStr.indexOf("\"");
-			var xslId = xslIdStr.substr(0,newpos);	//retrieving xslt_id using string operations from the response
-			var xmlUrl = "https://" + subdomain + ".bigmachines.com/admin/commerce/views/preview_xml.jsp?bs_id=" + tranId + "&xslt_id=" + xslId + "&view_type=document";
-			chrome.windows.create({url:xmlUrl});			
-		}
-	};
-	xhttp.open("GET",xslUrl,true);
-	xhttp.send();
+  port.onMessage.addListener(response => {
+    //Get Transaction and process IDs from content script
+    tranId = response.tid;
+    procId = response.pid;
+    document.getElementById("buysideid").value = tranId;
+  });
+});
+
+function openXml(xslUrl, subdomain) {
+  fetch(xslUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Fetching XSL Id failed.");
+      }
+      response.text().then(data => {
+        var toSearch = "edit_xslt.jsp?id=";
+        var pos = data.indexOf(toSearch);
+        var xslIdStr = data.substr(pos + toSearch.length, 50);
+        var newpos = xslIdStr.indexOf('"');
+        var xslId = xslIdStr.substr(0, newpos); //retrieving xslt_id using string operations from the response
+        var xmlUrl =
+          "https://" +
+          subdomain +
+          ".bigmachines.com/admin/commerce/views/preview_xml.jsp?bs_id=" +
+          tranId +
+          "&xslt_id=" +
+          xslId +
+          "&view_type=document";
+        chrome.windows.create({ url: xmlUrl });
+      });
+    })
+    .catch(error => {
+      console.error("Problem fetching XSL ID: ", error);
+    });
 }
-document.addEventListener("click",function(event) {
-	if(event.target.id === "copy") {
-		var copyText = document.getElementById("buysideid");
-		copyText.select();
-		document.execCommand("copy");
-	}
-	else if(event.target.id === "xml") {
-		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-		currentUrl = tabs[0].url;					//getting the current url of the active tab to retrieve the domain name
-		var pos = currentUrl.search(".bigmachines.com");
-		var pos1 = currentUrl.search("//");
-		var subdomain = currentUrl.slice(pos1+2,pos);		
-		xslUrl = "https://" + subdomain + ".bigmachines.com/admin/commerce/views/list_xslt.jsp?process_id=" + procId;
-		openXml(xslUrl,subdomain);	
-		});
-	}
-})
+
+document.addEventListener("click", function(event) {
+  if (event.target.id === "copy") {
+    var copyText = document.getElementById("buysideid");
+    copyText.select();
+    document.execCommand("copy");
+  } else if (event.target.id === "xml") {
+    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+      currentUrl = tabs[0].url; //getting the current url of the active tab to retrieve the domain name
+      var pos = currentUrl.search(".bigmachines.com");
+      var pos1 = currentUrl.search("//");
+      var subdomain = currentUrl.slice(pos1 + 2, pos);
+      xslUrl =
+        "https://" +
+        subdomain +
+        ".bigmachines.com/admin/commerce/views/list_xslt.jsp?process_id=" +
+        procId;
+      openXml(xslUrl, subdomain);
+    });
+  }
+});
