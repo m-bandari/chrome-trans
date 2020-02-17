7fa0d2c5680446ae2e5c04ad86d92da4ec24634b
Mahesh Bandari <11704574+m-bandari@users.noreply.github.com>
2020-02-17 12:07:24 +0530

    Added Support for JET UI

--- manifest.js
+++ manifest.js
@@ -1,17 +1,38 @@
-var tranId = getId();
-var procId = getProcId();
-chrome.runtime.sendMessage({			//sending message to background script
-	value: tranId, value2: procId
+var tranId;
+var procId;
+
+//Upon content script injection and establishing connection with popup
+chrome.runtime.onConnect.addListener(p => {
+  p.onMessage.addListener(message => {
+    if (message.isLegacy == "true") {
+      // fetching transaction ID and Process ID in case of Legacy UI
+      tranId = document.getElementsByName("id")[0].value;
+      procId = document.getElementsByName("bm_cm_process_id")[0].value;
+      p.postMessage({ tid: tranId, pid: procId });
+    } else if (message.isLegacy == "false" && message.tabUrl != "") {
+      // fetching transaction ID and Process ID in case of JET UI
+      var currentTabUrl = message.tabUrl;
+      var currentTabUrlFragmentsArr = currentTabUrl.split("/");
+      tranId = currentTabUrlFragmentsArr[currentTabUrlFragmentsArr.length - 1];
+      var processMetadataUrl =
+        "https://" +
+        currentTabUrlFragmentsArr[2] +
+        "/rest/v8/commerceProcesses/" +
+        currentTabUrlFragmentsArr[5] +
+        "/documents";
+      fetch(processMetadataUrl)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Fetching process documents failed.");
+          }
+          response.json().then(jsonData => {
+            procId = jsonData.items[0].process.id;
+          });
+        })
+        .catch(error => {
+          console.error("Problem fetching Process ID: ", error);
+        });
+      p.postMessage({ tid: tranId, pid: procId });
+    }
+  });
 });
-
-function getId() {			//To get transaction ID from the quote page
-  var input = document.getElementsByName("id");
-  var tranId = input[0].value;
-  return tranId;
-}
-
-function getProcId() {			//To get commerce process ID from the quote page
-	var input = document.getElementsByName("bm_cm_process_id");
-	var procId = input[0].value;
-	return procId;
-}
